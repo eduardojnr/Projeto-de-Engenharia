5165ea31955bd18cf53cd9c8a04c365a50760f4e
bllurryyk <khgds1@hotmail.com>
2021-09-19 02:08:02 -0300

    importando modulos do material

--- controle-experimentacao/src/app/app.module.ts
+++ controle-experimentacao/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { LoginComponent } from './screens/login/login.component';
 import { CadastroComponent } from './screens/cadastro/cadastro.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Material Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +21,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
