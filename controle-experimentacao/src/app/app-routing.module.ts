adc966209ab1007d11f2cd2d62549b590f3b7d3f
bllurryyk <khgds1@hotmail.com>
2021-09-19 01:27:47 -0300

    configurando rotas

--- controle-experimentacao/src/app/app-routing.module.ts
+++ controle-experimentacao/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Screens
+import { LoginComponent } from './screens/login/login.component';
+import { CadastroComponent } from './screens/cadastro/cadastro.component'
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
