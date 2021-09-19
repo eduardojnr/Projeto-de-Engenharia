0e4a774c2675fbacba87eebab9cc60729adb6170
bllurryyk <khgds1@hotmail.com>
2021-09-19 01:12:14 -0300

    instalando Angular Material

--- controle-experimentacao/src/app/app.module.ts
+++ controle-experimentacao/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './screens/login/login.component';
 import { CadastroComponent } from './screens/cadastro/cadastro.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CadastroComponent } from './screens/cadastro/cadastro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
