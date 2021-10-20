1531947490db9a25351dabb5ade93c3319c4ec42
GuilhermeVieiraMoraes <guims02@hotmail.com>
2021-10-20 11:35:51 -0300

    Tela recuperação de senha

--- front-end/controle-experimentacao/src/app/screens/recuperacao-senha/recuperacao-senha.component.ts
+++ front-end/controle-experimentacao/src/app/screens/recuperacao-senha/recuperacao-senha.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recuperacao-senha',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperacaoSenhaComponent implements OnInit {
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('rquired')) {
+      return 'Email invalido, favor digitar novamente';
+    }
+    return this.email.hasError('email') ? 'Email invalido, favor digitar novamente' : '';
   }
 
 }
