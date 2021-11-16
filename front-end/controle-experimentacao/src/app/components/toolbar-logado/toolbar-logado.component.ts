3fe18fe242710345e422cff6181dc61cfc142f7a
bllurryyk <khgds1@hotmail.com>
2021-11-16 05:44:55 -0300

    finalizando toolbar enquanto logado

--- front-end/controle-experimentacao/src/app/components/toolbar-logado/toolbar-logado.component.ts
+++ front-end/controle-experimentacao/src/app/components/toolbar-logado/toolbar-logado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 
 @Component({
   selector: 'app-toolbar-logado',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarLogadoComponent implements OnInit {
 
-  constructor() { }
+  user$ = this.usuarioService.retornaUsuario();
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+    ) { }
+
+  logout() {
+    this.usuarioService.logout();
+    this.router.navigate(['']);
+  }
 
   ngOnInit(): void {
   }
