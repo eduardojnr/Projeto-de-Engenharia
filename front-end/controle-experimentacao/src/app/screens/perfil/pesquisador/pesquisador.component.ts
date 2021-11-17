81faecc0758f46474ea0f101da45251198d2ec32
bllurryyk <khgds1@hotmail.com>
2021-11-17 01:14:56 -0300

    corrigindo tela com mal carregamento do bootstrap

--- front-end/controle-experimentacao/src/app/screens/perfil/pesquisador/pesquisador.component.ts
+++ front-end/controle-experimentacao/src/app/screens/perfil/pesquisador/pesquisador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pesquisador',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PesquisadorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  reloadPageAtualizacao(): void {
+    this.router.navigate(['/atualizarCadastro'])
+      .then(() => {
+        window.location.reload();
+      });
+  }
 }
